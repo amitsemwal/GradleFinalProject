07f370eb6a669d8cbb1274d8b6f4e614ded4a657
amitsemwal <semwal.amit1991@gmail.com>
2016-05-17 22:10:33 +0530

    Fixed Bug Button click not working

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -0,0 +1,66 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.jokedisplaylibrary.ui.JokeActivity;
+import com.udacity.gradle.builditbigger.network.FetchJokeTask;
+
+
+public class MainActivity extends AppCompatActivity implements FetchJokeTask.AsyncResponse {
+    private FetchJokeTask jokeTask;
+    private Intent jokeDisplayIntemt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        jokeTask = new FetchJokeTask();
+        jokeTask.setJokeResponce(this);
+        jokeDisplayIntemt = new Intent(this, JokeActivity.class);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically h
+        // andle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+    public void launchJokeActivity(View view){
+        jokeTask.execute();
+    }
+
+    @Override
+    public void handleJoke(String joke) {
+        if (joke == null) {
+            joke = getString(R.string.server_down_error);
+        }
+        jokeDisplayIntemt.putExtra(JokeActivity.JOKE_KEY, joke);
+        startActivity(jokeDisplayIntemt);
+    }
+}
---
+++ app/src/main/java/com/udacity/gradle/builditbigger/network/FetchJokeTask.java
@@ -0,0 +1,64 @@
+package com.udacity.gradle.builditbigger.network;
+
+import android.os.AsyncTask;
+
+import com.example.semwa.myapplication.backend.myApi.MyApi;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+
+import java.io.IOException;
+
+/**
+ * Created by semwa on 15-05-2016.
+ */
+public class FetchJokeTask extends AsyncTask<Void, Void, String> {
+    private static MyApi myApiService = null;
+    private AsyncResponse jokeResponce = null;
+
+    public void setJokeResponce(AsyncResponse jokeResponce) {
+        this.jokeResponce = jokeResponce;
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+        if (myApiService == null) {  // Only do this once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // options for running against local devappserver
+                    // - 10.0.2.2 is localhost's IP address in Android emulator
+                    // - turn off compression when running against local devappserver
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+            // end options for devappserver
+
+            myApiService = builder.build();
+        }
+
+
+        try {
+            String joke = myApiService.sendJoke().execute().getData();
+            return joke == null ? "bazinga" : joke;
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        jokeResponce.handleJoke(result);
+    }
+
+
+    public interface AsyncResponse {
+        void
+        handleJoke(String output);
+    }
+}
